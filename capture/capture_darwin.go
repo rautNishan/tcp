bec1496747aa46f5bc938ed387f61204daa41bf2
rautNishan <nishan.raut1709@gmail.com>
2026-04-20 20:44:22 +0545

    Open BPF device for darwin

--- capture/capture_darwin.go
+++ capture/capture_darwin.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"syscall"
+	"unsafe"
 
 	"golang.org/x/sys/unix"
 )
@@ -27,12 +29,29 @@ func Open() (int, error) {
 }
 
 // Doc: https://man7.org/linux/man-pages/man2/ioctl.2.html
+
 func SetImmediate(fd int) error {
 	val := 1
 	//Why unsafe.Pointer => Because Go’s type system normally prevents mixing pointer types, but syscalls require raw memory pointers (like C’s char *argp) (ioctl)
 	// More on unsafe.Pointer Doc: https://alexanderobregon.substack.com/p/unsafe-pointer-conversions-in-go
 	if err := unix.IoctlSetInt(fd, unix.BIOCIMMEDIATE, val); err != nil { //unsafe.Pointer internally used (https://github.com/seccome/Ehoney/blob/3712e644d326466a7d64b1dec937064c6f7db8d7/tool/go/src/runtime/sys_openbsd3.go#L4)
 		return fmt.Errorf("BIOCIMMEDIATE failed: %w", err)
+	}
+	return nil
+}
+
+func BindInterface(fd int, iface string) error {
+	var req ifreq
+	copy(req.Name[:], iface)
+
+	_, _, errno := unix.Syscall(
+		syscall.SYS_IOCTL,
+		uintptr(fd),
+		uintptr(unix.BIOCSETIF),
+		uintptr(unsafe.Pointer(&req)),
+	)
+	if errno != 0 {
+		return fmt.Errorf("BIOCSETIF failed: %w", errno)
 	}
 	return nil
 }
--- capture/main.go
+++ capture/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/sys/unix"
+)
 
 func main() {
-	fmt.Println("Packets")
+	iface := "en0" // default interface; change to "en1", "utun0", etc. if needed
+	if len(os.Args) > 1 {
+		iface = os.Args[1]
+	}
+	fmt.Println("Used interface: ", iface)
+	fd, err := Open() //https://docs.oracle.com/cd/E36784_01/html/E36884/bpf-7d.html
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error: ", err)
+		os.Exit(1)
+	}
+	fmt.Println("This is bfp fd: ", fd)
+	// _ = readFD(fd)
+
+}
+
+// Just seeing what is inside this fd.
+func readFD(fd int) error {
+	buf := make([]byte, 4096)
+	n, err := unix.Read(fd, buf)
+	if err != nil {
+		fmt.Println("Error: ", err) //(Error:  device not configured) This is the erro we will get
+		// because  the BPF device is opened but never
+		// bound it to a network interface.
+		// A BPF fd must be attached to an interface before it can capture packets.
+		// (//https://docs.oracle.com/cd/E36784_01/html/E36884/bpf-7d.html)
+		return err
+	}
+	fmt.Println(string(buf[:n]))
+	return nil
 }
